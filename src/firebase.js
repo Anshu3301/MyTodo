2024136929e3bced2f1dcccd7d0bb38387be209c
Anshu3301 <sainanshu40@gmail.com>
2025-07-03 17:13:38 +0530

    added .env file

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCIfZHX_WXEuL6X5c2K7eK89VMktk5GZYI",
-  authDomain: "todo-app-71d48.firebaseapp.com",
-  projectId: "todo-app-71d48",
-  storageBucket: "todo-app-71d48.firebasestorage.app",
-  messagingSenderId: "186727760691",
-  appId: "1:186727760691:web:30f16903a77d3071c87446",
-  measurementId: "G-TB2BQ31ZEW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
